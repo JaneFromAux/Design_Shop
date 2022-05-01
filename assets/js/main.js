a1074a3fad3aa842a0940adfd8d8d8097b5093aa
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-01 22:17:18 +0200

    pop up menu added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 let slideIn = document.getElementById("slideIn");
+// let closeBar = document.getElementById("closeBar");
 
 function slideMenu() {
-    console.log('funktion runs');
+    console.log('menu slides');
     slideIn.classList.add("slide");
+}
+function closeBar() {
+    console.log('bar closed');
+    slideIn.classList.remove("slide");
 }
